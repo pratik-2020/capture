0829a5947b84f4d29c242db55d306003f7662b7c
Pratik Singh Thakur <pratikthakur2019@gmail.com>
2022-01-10 16:11:54 +0530

    added the cors again

--- src/Component/SignIn.js
+++ src/Component/SignIn.js
@@ -16,7 +16,7 @@ function SignIn(){
     const [user, setUser] = useState("");
 
     const handlesignin = () => {
-        axios.post('https://capture-img-server.herokuapp.com/otpverification', {
+        axios.post('http://localhost:3304/otpverification', {
             user: user,
             pass: password,
             state: state,
@@ -34,6 +34,22 @@ function SignIn(){
             }
         })
     }
+    (function() {
+        var cors_api_host = 'cors-anywhere.herokuapp.com';
+        var cors_api_url = 'https://' + cors_api_host + '/';
+        var slice = [].slice;
+        var origin = window.location.protocol + '//' + window.location.host;
+        var open = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function() {
+            var args = slice.call(arguments);
+            var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+            if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+                targetOrigin[1] !== cors_api_host) {
+                args[1] = cors_api_url + args[1];
+            }
+            return open.apply(this, args);
+        };
+    })();
     return(
         <div className="container">
             
